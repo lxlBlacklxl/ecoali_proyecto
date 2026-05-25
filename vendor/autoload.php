55769addcf5b246c6c05caa1289634cf8a5d4645
Jorge Luis <jorge_gnk12149@hotmail.com>
2026-05-25 13:17:31 -0600

    fix: restore complete vendor autoload files and remove platform_check

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2185d2f99bcd56787481d9357a5972d3::getLoader();
+return ComposerAutoloaderInitbc46f9932d524e2ace416af5576c5288::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
+class ComposerStaticInitbc46f9932d524e2ace416af5576c5288
 {
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
@@ -139,9 +139,9 @@ class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitbc46f9932d524e2ace416af5576c5288::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitbc46f9932d524e2ace416af5576c5288::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitbc46f9932d524e2ace416af5576c5288::$classMap;
 
         }, null, ClassLoader::class);
     }
